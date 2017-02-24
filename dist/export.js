766a0b6d0ece0caad417d162dfbfd28afa8499f1
Dominik Angerer <dominikangerer1@gmail.com>
2017-02-24 10:09:42 +0100

    added builded version

--- dist/export.js
+++ dist/export.js
@@ -1,26 +1,4 @@
 (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var inserted = exports.cache = {}
-
-exports.insert = function (css) {
-  if (inserted[css]) return
-  inserted[css] = true
-
-  var elem = document.createElement('style')
-  elem.setAttribute('type', 'text/css')
-
-  if ('textContent' in elem) {
-    elem.textContent = css
-  } else {
-    elem.styleSheet.cssText = css
-  }
-
-  document.getElementsByTagName('head')[0].appendChild(elem)
-  return elem
-}
-
-},{}],2:[function(require,module,exports){
-var __vueify_insert__ = require("vueify/lib/insert-css")
-var __vueify_style__ = __vueify_insert__.insert(".p-metatags__google-title{color:blue;text-decoration:underline}.p-metatags__google-link{color:green}.p-metatags__preview{margin:5px 0 15px;padding:10px;color:#000;background:#fff}")
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
@@ -28,26 +6,73 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.default = {
   computed: {
-    charCount: function charCount() {
-      return this.model.description.length;
+    loading: function loading() {
+      return !this.model.tags.length;
+    },
+    options: function options() {
+      var options = {};
+      for (var index = 0; index < this.schema.options.length; index++) {
+        var element = this.schema.options[index];
+        options[element.name] = element.value;
+      }
+      return options;
     }
   },
   mixins: [window.Storyblok.plugin],
   methods: {
     initWith: function initWith() {
       return {
-        plugin: 'metatags',
-        title: 'Your title',
-        description: 'Your description'
+        plugin: 'roomle-tags',
+        tags: [],
+        type: '',
+        value: '',
+        values: []
       };
+    },
+    concatUnique: function concatUnique(array, b) {
+      var a = array.concat(b);
+      for (var i = 0; i < a.length; ++i) {
+        for (var j = i + 1; j < a.length; ++j) {
+          if (a[i] === a[j]) a.splice(j--, 1);
+        }
+      }
+      return a;
     }
   },
   events: {
     'plugin:created': function pluginCreated() {
-      console.log('plugin:created');
-    },
-    'plugin:destroyed': function pluginDestroyed() {
-      console.log('plugin:destroyed');
+      var _this = this;
+
+      // Make sure Options are set.
+      if (!this.schema.options) {
+        console.warn('roomle-tags: We guess you\'re working locally so we will set the options for you.');
+        this.schema.options = [{
+          name: "root_tag",
+          value: "austrianfurniture_0_root"
+        }, {
+          name: "type",
+          value: "single"
+        }];
+      }
+
+      // Check if min 2
+      if (this.schema.options.length < 2) {
+        console.error('roomle-tags: Define the following options: root_tag -> string and type -> string (single|multi)');
+        return false;
+      }
+
+      // Set Type
+      this.model.type = this.options.type ? this.options.type : 'single';
+
+      // load Data from Roomle
+      jQuery.ajax({
+        url: 'https://api.roomle.com/v2/tags/' + this.options.root_tag + '/',
+        success: function success(response) {
+          var responseTags = response.tag.tags;
+          var currentTags = _this.model.tags;
+          _this.model.tags = _this.concatUnique(currentTags, responseTags);
+        }
+      });
     }
   },
   watch: {
@@ -60,9 +85,9 @@ exports.default = {
   }
 };
 if (module.exports.__esModule) module.exports = module.exports.default
-;(typeof module.exports === "function"? module.exports.options: module.exports).template = "<div>Google snippet preview:<div class=p-metatags__preview><div class=p-metatags__google-title>{{ model.title }}</div><div class=p-metatags__google-link>yoursite.com/example</div><div class=p-metatags__google-description>{{ model.description }}</div></div><div class=uk-form-row><label>SEO Title</label><input type=text v-model=model.title class=uk-width-1-1></div><div class=uk-form-row><label>Meta description</label><textarea rows=4 v-model=model.description class=uk-width-1-1></textarea><div>{{ charCount }} of 156 chars used</div></div></div>"
+;(typeof module.exports === "function"? module.exports.options: module.exports).template = "<div><div v-if=loading>Loading...</div><div class=uk-form-row v-if=\"model.type == 'single'\"><select class=uk-width-1-1 v-model=model.value><option><option v-for=\"tag in model.tags\" v-bind:value=tag>{{ tag }}</select></div><div class=uk-form-row v-else=\"\"><label class=\"uk-margin-right uk-margin-bottom uk-display-block\" v-for=\"tag in model.tags\"><input v-model=model.values type=checkbox class=uk-margin-small-right value=\"{{ tag }}\"> {{ tag }}</label></div></div>"
 
-},{"vueify/lib/insert-css":1}],3:[function(require,module,exports){
+},{}],2:[function(require,module,exports){
 'use strict';
 
 var _Plugin = require('../Plugin.vue');
@@ -75,4 +100,4 @@ var init = _Plugin2.default.methods.initWith();
 
 window.storyblok.field_types[init.plugin] = _Plugin2.default;
 
-},{"../Plugin.vue":2}]},{},[3]);
+},{"../Plugin.vue":1}]},{},[2]);
